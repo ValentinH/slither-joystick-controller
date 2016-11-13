dcc6b77fe493be20f0848e17b4d7d03827e8fb20
Valentin Hervieu <valentin@hervi.eu>
2016-11-13 15:49:36 +0100

    Clean code and improve documentation

--- README.md
+++ README.md
@@ -13,6 +13,24 @@ Simple script to bind joystick controller to Slither mouse events
 npm i slither-joystick-controller
 ```
 
+## How to run
+
+1) go in the `slither-joystick-controller` folder
+2) run `node index.js` or `node index.js supported/controller/here`
+
+### Supported Controllers
+- ps3/dualshock3 (default)
+- ps4/dualshock4
+- snes/tomee
+- snes/retrolink
+- n64/retrolink
+- logitech/rumblepad2
+- logitech/dualaction
+- logitech/gamepadf710
+- microsoft/sidewinder-precision-2
+
+These controllers are handled by [node-gamepad](https://github.com/carldanley/node-gamepad). If you need to add another controller, see the [Contributing Controllers](https://github.com/carldanley/node-gamepad#contributing-controllers) of `node-gamepad`.
+
 ## How to use
 
 Game pad action | Slither action
@@ -24,3 +42,9 @@ triangle | `enter` - use to start the game
 L2 | `zoom out` (require [Slither plus](https://chrome.google.com/webstore/detail/slitherplus-zoom-skin-cre/cpbghpalffgmgocmnigfhalghmaemffo) extension)
 R2 | `zoom in` (require [Slither plus](https://chrome.google.com/webstore/detail/slitherplus-zoom-skin-cre/cpbghpalffgmgocmnigfhalghmaemffo) extension)
 select | redefine center position. It can be useful when the game is not in full screen. Place your mouse on the snake head and press `select` to set the center.
+
+## Libraries used in this project
+
+- [node-gamepad](https://github.com/carldanley/node-gamepad) - for reading controller inputs
+- [robotjs](https://github.com/octalmage/robotjs) - for firing mouse/keyboards events
+- [node-notifier](https://github.com/mikaelbr/node-notifier) - for displaying notifications
--- index.js
+++ index.js
@@ -2,84 +2,105 @@ var GamePad = require('node-gamepad'),
   robot = require("robotjs"),
   notifier = require('node-notifier')
 
-var screenSize = robot.getScreenSize()
-var center = { x: screenSize.width / 2, y: screenSize.height / 2 }
+var screenSize = robot.getScreenSize(),
+  center = { x: screenSize.width / 2, y: screenSize.height / 2 }
 
-var LOW_THRESHOLD = 5,
+var DEFAULT_CONTROLLER = 'ps3/dualshock3',
+  LOW_THRESHOLD = 5,
   HIGH_THRESHOLD = 250,
   MAX_MOVE_OFFSET = 100,
+  MOUSE_X = center.x,
+  MOUSE_Y = center.y,
   L2_PRESSED = false,
   R2_PRESSED = false,
   CONTROLLER_PAUSED = false
 
-var controller = new GamePad('ps3/dualshock3')
-controller.connect()
+var controller_type = DEFAULT_CONTROLLER
+var args = process.argv.slice(2)
+if (args.length > 0)
+  controller_type = args[0]
+
+var controller = new GamePad(controller_type)
+try {
+  controller.connect()
+}
+catch (e) {
+  throw new Error('Unable to connect to a controller of type ' + controller_type)
+}
 
 //this is the main loop that is holding press event
-setInterval(function() {
-  if (CONTROLLER_PAUSED)
-    return
-  if (L2_PRESSED)
-    robot.scrollMouse(1, "up");
-  if (R2_PRESSED)
-    robot.scrollMouse(1, "down");
-}, 16)
+startEventLoop()
+bindControllerListeners()
 
-controller.on('left:move', function(data) {
-  if (CONTROLLER_PAUSED)
-    return
-  if (data.x > LOW_THRESHOLD && data.x < HIGH_THRESHOLD && data.y > LOW_THRESHOLD && data.y < HIGH_THRESHOLD)
-    return
+function startEventLoop () {
+  return setInterval(function() {
+    if (CONTROLLER_PAUSED)
+      return
+    if (L2_PRESSED)
+      robot.scrollMouse(1, 'up')
+    if (R2_PRESSED)
+      robot.scrollMouse(1, 'down')
 
-  var mouseX = mapJoystickToPixels(data.x, center.x),
-    mouseY = mapJoystickToPixels(data.y, center.y)
+    robot.moveMouse(MOUSE_X, MOUSE_Y)
+  }, 16)
+}
 
-  robot.moveMouse(mouseX, mouseY);
-})
+function bindControllerListeners () {
+  controller.on('left:move', function(data) {
+    if (CONTROLLER_PAUSED)
+      return
+    if (data.x > LOW_THRESHOLD && data.x < HIGH_THRESHOLD && data.y > LOW_THRESHOLD && data.y < HIGH_THRESHOLD)
+      return
+    MOUSE_X = mapJoystickToPixels(data.x, center.x)
+    MOUSE_Y = mapJoystickToPixels(data.y, center.y)
+  })
 
-controller.on('x:press', function() {
-  if (CONTROLLER_PAUSED)
-    return
-  robot.mouseToggle('down', 'left');
-})
+  controller.on('x:press', function() {
+    if (CONTROLLER_PAUSED)
+      return
+    robot.mouseToggle('down', 'left')
+  })
 
-controller.on('x:release', function() {
-  robot.mouseToggle('up', 'left');
-})
+  controller.on('x:release', function() {
+    robot.mouseToggle('up', 'left')
+  })
 
-controller.on('triangle:press', function() {
-  if (CONTROLLER_PAUSED)
-    return
-  robot.keyTap('enter');
-})
+  controller.on('triangle:press', function() {
+    if (CONTROLLER_PAUSED)
+      return
+    robot.keyTap('enter')
+  })
 
-controller.on('l2:press', function() {
-  L2_PRESSED = true
-})
+  controller.on('l2:press', function() {
+    L2_PRESSED = true
+  })
 
-controller.on('r2:press', function() {
-  R2_PRESSED = true
-})
+  controller.on('r2:press', function() {
+    R2_PRESSED = true
+  })
 
-controller.on('l2:release', function() {
-  L2_PRESSED = false
-})
+  controller.on('l2:release', function() {
+    L2_PRESSED = false
+  })
 
-controller.on('r2:release', function() {
-  R2_PRESSED = false
-})
+  controller.on('r2:release', function() {
+    R2_PRESSED = false
+  })
 
-controller.on('start:press', function() {
-  CONTROLLER_PAUSED = !CONTROLLER_PAUSED
-  if(CONTROLLER_PAUSED)
-    notify('Paused')
-  else
-    notify('Listening')
-})
+  controller.on('start:press', function() {
+    CONTROLLER_PAUSED = !CONTROLLER_PAUSED
+    if (CONTROLLER_PAUSED)
+      notify('Paused')
+    else
+      notify('Listening')
+  })
 
-controller.on('select:press', function() {
-  center = robot.getMousePos()
-})
+  controller.on('select:press', function() {
+    if (CONTROLLER_PAUSED)
+      return
+    center = robot.getMousePos()
+  })
+}
 
 function mapJoystickToPixels (joystickPos, screenStartPos) {
   var offset = scale(joystickPos, 0, 255, -MAX_MOVE_OFFSET, MAX_MOVE_OFFSET)
@@ -88,13 +109,13 @@ function mapJoystickToPixels (joystickPos, screenStartPos) {
 }
 
 function scale (valueIn, baseMin, baseMax, limitMin, limitMax) {
-  return ((limitMax - limitMin) * (valueIn - baseMin) / (baseMax - baseMin)) + limitMin;
+  return ((limitMax - limitMin) * (valueIn - baseMin) / (baseMax - baseMin)) + limitMin
 }
 
-function notify(msg) {
+function notify (msg) {
   notifier.notify({
     title: 'Slither Controller',
     message: msg,
     timeout: 2
-  });
+  })
 }
